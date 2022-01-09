2816164f3b72efa3424dbf4fc97aebb550f4768d
Moritz <cookie.moehre@gmail.com>
2022-01-09 18:26:38 +0100

    Install script done.

--- get-anki.sh
+++ get-anki.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 LATESTPATH="https://github.com/ankitects/anki/releases/latest"
+DOWNLOADPATH="https://github.com/ankitects/anki/releases/download/"
 
 METHOD=0
 # 0: update
@@ -24,6 +25,21 @@ else
 	exit 1
 fi
 
+install_anki () {
+	INSTALLNAME="anki-${REMOTEVERSION}-linux"
+	cd "/tmp"
+	wget -q -O "${INSTALLNAME}.tar.bz2" --show-progress "${DOWNLOADPATH}${REMOTEVERSION}/${INSTALLNAME}.tar.bz2"
+	echo -n "Decompressing... "
+	tar xjf "${INSTALLNAME}.tar.bz2"
+	echo "Done."
+	cd "$INSTALLNAME"
+	if [ "$(whoami)" == "root" ]; then
+		./install.sh
+	else
+		sudo ./install.sh
+	fi
+}
+
 REMOTEURL=$(wget --spider "$LATESTPATH" 2>&1 | grep "Location:" | cut -d " " -f 2)
 REMOTEVERSION=$(echo "$REMOTEURL" | rev | cut -d "/" -f 1 | rev)
 if [ $METHOD -lt 2 ]; then
@@ -37,9 +53,24 @@ if [ $METHOD -lt 2 ]; then
 	echo "Installed Anki Version: $LOCALVERSION"
 	echo "Latest Anki Version: $REMOTEVERSION"
 	if [ $RV1 -gt $LV1 ] || [[ $RV1 -ge $LV1 && $RV2 -gt $LV2 ]] || [[ $RV1 -ge $LV1 && $RV2 -ge $LV2 && $RV3 -gt $LV3 ]]; then
-		echo Update
+		if [ $METHOD == "0" ]; then
+			read -p "Do you want to continue? <y>: " ASK
+			if [ $ASK != "y" ]; then
+				echo "Aborted."
+				exit 0
+			fi
+		fi
+		install_anki
 	fi
 else
-	echo Install
+	echo "Latest Anki Version: $REMOTEVERSION"
+	if [ $METHOD == "2" ]; then
+		read -p "Do you want to continue? <y>: " ASK
+		if [ $ASK != "y" ]; then
+			echo "Aborted."
+			exit 0
+		fi
+	fi
+	install_anki
 fi
 
