09e0ffa57d998021efabc614edf8422ad3d6ed2c
Moritz <cookie.moehre@gmail.com>
2022-01-09 17:53:10 +0100

    Basic functions

--- get-anki.sh
+++ get-anki.sh
@@ -1,3 +1,45 @@
-#!/usr/bash
-echo Test
+#!/usr/bin/bash
+
+LATESTPATH="https://github.com/ankitects/anki/releases/latest"
+
+METHOD=0
+# 0: update
+# 1: forced update
+# 2: install
+# 3: forced-install
+ANKI=$(which anki)
+
+if [ -z "$1" ]; then
+	if [ -f "$ANKI" ]; then
+		METHOD=0
+	else
+		METHOD=2
+	fi
+elif [ "$1" == "update" ]; then
+	METHOD=1
+elif [ "$1" == "install" ]; then
+	METHOD=3
+else
+	echo "Unknown option."
+	exit 1
+fi
+
+REMOTEURL=$(wget --spider "$LATESTPATH" 2>&1 | grep "Location:" | cut -d " " -f 2)
+REMOTEVERSION=$(echo "$REMOTEURL" | rev | cut -d "/" -f 1 | rev)
+if [ $METHOD -lt 2 ]; then
+	LOCALVERSION=$("$ANKI" -v | cut -d " " -f 2)
+	RV1=$(echo "$REMOTEVERSION" | cut -d "." -f 1)
+	RV2=$(echo "$REMOTEVERSION" | cut -d "." -f 2)
+	RV3=$(echo "$REMOTEVERSION" | cut -d "." -f 3)
+	LV1=$(echo "$LOCALVERSION" | cut -d "." -f 1)
+	LV2=$(echo "$LOCALVERSION" | cut -d "." -f 2)
+	LV3=$(echo "$LOCALVERSION" | cut -d "." -f 3)
+	echo "Installed Anki Version: $LOCALVERSION"
+	echo "Latest Anki Version: $REMOTEVERSION"
+	if [ $RV1 -gt $LV1 ] || [[ $RV1 -ge $LV1 && $RV2 -gt $LV2 ]] || [[ $RV1 -ge $LV1 && $RV2 -ge $LV2 && $RV3 -gt $LV3 ]]; then
+		echo Update
+	fi
+else
+	echo Install
+fi
 
